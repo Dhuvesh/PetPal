14ff67b2d5e43963459f319b02fed447a801b9c7
Dhuvesh <dhuvesh.salian@gmail.com>
2025-08-10 20:09:12 +0530

    vercel 6 changes

--- Server/index.js
+++ Server/index.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://pet-pal-five.vercel.app/login",
+    origin: "https://pet-pal-five.vercel.app",
     credentials: true 
 }));
 
