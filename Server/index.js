841940577ed2a098a8e14cb70b04afba49db2a03
Dhuvesh <dhuvesh.salian@gmail.com>
2025-11-30 20:22:53 +0530

    deploying to production

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://pet-pal-five.vercel.app",
         methods: ["GET", "POST"]
     }
 });
@@ -36,7 +36,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://pet-pal-five.vercel.app",
     credentials: true 
 }));
 
